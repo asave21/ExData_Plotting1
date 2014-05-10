b2c2e3dd3dd4c9806d87c309413fe93d5b8a2222
Ankur Save <asave21@gmail.com>
2014-05-10 23:37:56 +0530

    Plot 1
    Final

--- plot1.r
+++ plot1.r
@@ -1,5 +1,5 @@
 png('plot1.png')
-data = read.table("household_power_consumption.txt", header = TRUE,sep = ";")
+data = read.table("household_power_consumption.txt", header = TRUE,sep = ";",na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 data = subset(data, Date == "1/2/2007" | Date == "2/2/2007")
-hist((as.numeric(as.character(data$Global_active_power))),col = "red",xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+hist(data$Global_active_power,col = "red",xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
---
+++ plot2.r
@@ -0,0 +1,6 @@
+png('plot2.png')
+data = read.table("household_power_consumption.txt", header = TRUE,sep = ";")
+data = subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+data$Date = strptime(data$Date,"%d/%m/%y")
+data$Date = as.Date(data$Date)
+dev.off()
