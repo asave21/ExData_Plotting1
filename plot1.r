203b8006f21a4001f0a95fd05a610f05be5938b6
Ankur Save <asave21@gmail.com>
2014-05-10 23:43:08 +0530

    Plot 1 _v2
    minor changes

--- plot1.r
+++ plot1.r
@@ -1,4 +1,4 @@
-png('plot1.png')
+png('plot1.png',width = 480, height = 480, units = "px")
 data = read.table("household_power_consumption.txt", header = TRUE,sep = ";",na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 data = subset(data, Date == "1/2/2007" | Date == "2/2/2007")
 hist(data$Global_active_power,col = "red",xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
